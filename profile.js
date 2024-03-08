38fef7c07ebe238343bbdc96bcc1eff0919277e4
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-03-08 14:59:46 -0800

    core finished. extra cred button still not finished.

--- profile.js
+++ profile.js
@@ -8,29 +8,31 @@
 const color = document.querySelector('#color')
 const place = document.querySelector('#place')
 const ritual = document.querySelector('#ritual')
-
-// create variable to track resto recommendation
 const restoRec = document.querySelector('.restoRec')
+
+
 
 // write the function to add buttons alerts
 
 const colorBtn = () => {
     alert(`My favorite color is YELLOW!`)
 }
-color.addEventListener('click', colorBtn)
 
 const placeBtn = () => {
     alert(`My favorite place is the canopy`)
 }
-place.addEventListener('click', placeBtn)
 
 const ritualBtn = () => {
     alert(`Rituals are best kept secret`)
 }
-ritual.addEventListener('click', ritualBtn)
 
-// create function to get a random restaraunt
 const resto = () => {
-    alert(`it works`)
+    console.log('something')
+    alert(`test`)
 }
+
+// add the event listeners for click functions
+color.addEventListener('click', colorBtn)
+place.addEventListener('click', placeBtn)
+ritual.addEventListener('click', ritualBtn)
 restoRec.addEventListener('click', resto)
