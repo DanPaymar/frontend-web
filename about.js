07c93aafed1580b822f90edfdddb77ffd93cf653
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-03-08 11:25:39 -0800

    Finished besides extra cred

--- about.js
+++ about.js
@@ -6,9 +6,19 @@ function handleSubmit(evt) {
 	evt.preventDefault();
 	
 	console.log('form submit');
+
+	alert(`Look at you, you submitted a form.`)
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const homeImg = document.querySelector('img')
+// create the compliment function with an alert
+const compliment = () => {
+	alert(`You have good geometry`)
+}
+// add event listener to trigger alert on mouseover
+homeImg.addEventListener('mouseover', compliment)
---
+++ profile.js
@@ -0,0 +1,32 @@
+// 
+
+// In profile.js, create event listeners that check for a click on each of these buttons.
+// 
+// If a user clicks on “My Favorite Color”, it should alert the user of your favorite color, and so on.
+
+// create variables to query each button individually
+const color = document.querySelector('#color')
+const place = document.querySelector('#place')
+const ritual = document.querySelector('#ritual')
+
+
+
+// write the function to add buttons alerts
+
+const colorBtn = () => {
+    alert(`My favorite color is YELLOW!`)
+}
+
+const placeBtn = () => {
+    alert(`My favorite place is the canopy`)
+}
+
+const ritualBtn = () => {
+    alert(`Rituals are best kept secret`)
+}
+
+// add event listeners to each button to trigger aleart
+
+color.addEventListener('click', colorBtn)
+place.addEventListener('click', placeBtn)
+ritual.addEventListener('click', ritualBtn)
